45e94da39cf9f83269598f08db4d20c20ceca6a5
gzarate <gzarate@HXNB-1967>
2023-07-23 09:13:30 -0500

    Added PORT environment variable to deploy.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
     credentials: true,
   });
   app.use(cookieParser())
-  await app.listen(5000);
+  const config = app.get(ConfigService)
+  await app.listen(config.get("PORT"));
 }
 bootstrap();
